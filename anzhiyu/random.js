8ee05647cd6d0497a89f7530d918b8c49190c460
zy111x <1563372518@qq.com>
2024-07-14 17:25:11 +0800

    Site updated: 2024-07-14 17:25:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/14/hello-world/"];function toRandomPost(){
+var posts=["2024/07/14/hello-world/","2024/07/11/搭建个人hexo博客/","2024/07/11/telegraph-Image/","2024/07/07/Markdown-teaching/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
