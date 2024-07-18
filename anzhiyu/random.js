4ab6534642f844f02eee3bd3c198a5b024982059
zy111x <1563372518@qq.com>
2024-07-18 16:33:46 +0800

    Site updated: 2024-07-18 16:33:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/14/hello-world/","2024/07/11/搭建个人hexo博客/","2024/07/11/telegraph-Image/","2024/07/07/Markdown-teaching/","2023/07/14/油猴插件的安装与使用/"];function toRandomPost(){
+var posts=["2024/07/17/非黑即白/","2024/07/14/hello-world/","2024/07/11/搭建个人hexo博客/","2024/07/11/telegraph-Image/","2024/07/07/Markdown-teaching/","2023/07/14/油猴插件的安装与使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
