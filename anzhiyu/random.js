c2bdb22f3f2af065d942b4b32af4f2efe12d8a76
zy111x <yxzhou629@gmail.com>
2024-07-27 16:47:46 +0800

    Site updated: 2024-07-27 16:47:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/17/非黑即白/","2024/07/14/hello-world/","2024/07/11/搭建个人hexo博客/","2024/07/11/telegraph-Image/","2024/07/07/Markdown-teaching/","2023/07/14/油猴插件的安装与使用/"];function toRandomPost(){
+var posts=["posts/d50a/","posts/58960/","posts/16107/","posts/32968/","posts/2729/","posts/22648/","posts/14307/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
