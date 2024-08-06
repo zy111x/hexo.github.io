49af5b55cc2af65f6a9230b8dad85e186c951020
zy111x <yxzhou629@gmail.com>
2024-08-06 23:04:56 +0800

    Site updated: 2024-08-06 23:04:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/d50a/","posts/5548/","posts/47193/","posts/58960/","posts/16107/","posts/32968/","posts/2729/","posts/22648/","posts/14307/"];function toRandomPost(){
+var posts=["posts/5548/","posts/47193/","posts/58960/","posts/16107/","posts/32968/","posts/2729/","posts/22648/","posts/14307/","posts/d50a/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
