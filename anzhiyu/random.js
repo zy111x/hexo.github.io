496977fccb0c190b485ad8071aa736ca170f7d5d
zy111x <yxzhou629@gmail.com>
2024-08-07 00:31:12 +0800

    Site updated: 2024-08-07 00:31:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/5548/","posts/58960/","posts/16107/","posts/32968/","posts/2729/","posts/22648/","posts/14307/","posts/d50a/"];function toRandomPost(){
+var posts=["posts/d50a/","posts/5548/","posts/58960/","posts/16107/","posts/32968/","posts/2729/","posts/22648/","posts/14307/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
