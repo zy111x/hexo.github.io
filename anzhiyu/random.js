392fd6ff370d04c125b1aeda49d6ebac479e122a
zy111x <yxzhou629@gmail.com>
2024-10-01 01:11:35 +0800

    Site updated: 2024-10-01 01:11:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["posts/32968/","posts/d50a/","posts/14833/","posts/53880/","posts/14375/","posts/63922/","posts/19367/","posts/14373/","posts/9/","posts/5548/","posts/58960/","posts/16107/","posts/2729/","posts/22648/","posts/14307/"];function toRandomPost(){
+var posts=["posts/9/","posts/32968/","posts/d50a/","posts/14833/","posts/53880/","posts/14375/","posts/63922/","posts/19367/","posts/14373/","posts/5548/","posts/58960/","posts/16107/","posts/2729/","posts/22648/","posts/14307/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
